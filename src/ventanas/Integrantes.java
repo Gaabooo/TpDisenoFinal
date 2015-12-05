57eabf79cc1c725dc53ff34d143351b30a3e106b
GabrielV <GabrielV@Gabriel.Home>
2015-12-05 13:41:23 -0300

    el gabe comitajeando pim

---
+++ src/ventanas/Integrantes.java
@@ -0,0 +1,169 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ventanas;
+
+import modelo.CompetenciaAux;
+
+/**
+ *
+ * @author GabrielV
+ */
+public class Integrantes extends javax.swing.JPanel {
+    
+    CompetenciaAux compAux;
+    
+    /**
+     * Creates new form AltaParticipante
+     */
+    public Integrantes(CompetenciaAux param) {
+        compAux=param;
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jButton4 = new javax.swing.JButton();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jButton5 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+
+        setMaximumSize(new java.awt.Dimension(800, 600));
+        setMinimumSize(new java.awt.Dimension(800, 600));
+        setLayout(null);
+
+        jLabel1.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel1.setText("Correo Electrónico:");
+        add(jLabel1);
+        jLabel1.setBounds(350, 200, 110, 22);
+
+        jLabel2.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel2.setText("Nombre y Apellido:");
+        add(jLabel2);
+        jLabel2.setBounds(350, 230, 110, 30);
+
+        jButton2.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jButton2.setText("Atras");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        add(jButton2);
+        jButton2.setBounds(136, 530, 70, 31);
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/img_avatar.jpg"))); // NOI18N
+        add(jLabel3);
+        jLabel3.setBounds(41, 119, 290, 290);
+
+        jButton4.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jButton4.setText("Menu P.");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+        add(jButton4);
+        jButton4.setBounds(41, 530, 86, 31);
+
+        jLabel5.setFont(new java.awt.Font("Agency FB", 0, 36)); // NOI18N
+        jLabel5.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setText("Alta de Participante");
+        add(jLabel5);
+        jLabel5.setBounds(1, 2, 800, 60);
+
+        jLabel6.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel6.setText("Correo Electrónico: eldeale@outlook.com");
+        add(jLabel6);
+        jLabel6.setBounds(350, 130, 110, 22);
+
+        jLabel7.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel7.setText("Nombre y Apellido:");
+        add(jLabel7);
+        jLabel7.setBounds(350, 160, 110, 30);
+
+        jLabel8.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel8.setText("Correo Electrónico:");
+        add(jLabel8);
+        jLabel8.setBounds(350, 280, 110, 22);
+
+        jLabel9.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel9.setText("Nombre y Apellido:");
+        add(jLabel9);
+        jLabel9.setBounds(350, 310, 110, 30);
+
+        jLabel10.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel10.setText("Correo Electrónico:");
+        add(jLabel10);
+        jLabel10.setBounds(350, 350, 110, 22);
+
+        jLabel11.setFont(new java.awt.Font("Agency FB", 0, 18)); // NOI18N
+        jLabel11.setText("Nombre y Apellido:");
+        add(jLabel11);
+        jLabel11.setBounds(350, 380, 110, 30);
+
+        jButton5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/sports logo released.png"))); // NOI18N
+        jButton5.setBorder(null);
+        jButton5.setBorderPainted(false);
+        jButton5.setContentAreaFilled(false);
+        jButton5.setOpaque(false);
+        jButton5.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton5ActionPerformed(evt);
+            }
+        });
+        add(jButton5);
+        jButton5.setBounds(650, 540, 150, 60);
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/img_general.jpg"))); // NOI18N
+        add(jLabel4);
+        jLabel4.setBounds(0, 0, 800, 600);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        V.get().remove(this);
+        V.get().listarParticipantesSalir();
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        V.get().remove(this);
+        V.get().listarParticipantesVolver();
+    }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton5ActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
+    // End of variables declaration//GEN-END:variables
+}
